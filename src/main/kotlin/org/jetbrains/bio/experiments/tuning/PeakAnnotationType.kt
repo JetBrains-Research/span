17eb6c47b587fe1647702cfd20a7bbaefdc4a28d
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2018-06-20 12:12:05 +0300

    1) Reorder labels buttons #1266 2) Ann buttons length changed to fit all 4 buttons in minimal frame width

--- src/main/kotlin/org/jetbrains/bio/experiments/tuning/PeakAnnotationType.kt
+++ src/main/kotlin/org/jetbrains/bio/experiments/tuning/PeakAnnotationType.kt
@@ -7,18 +7,21 @@ enum class PeakAnnotationType(val id: String,
                               val help: String,
                               val color: Color) {
     // Use colors from original markup (see article)
-    PEAK_START("peakStart",
-               "Peak Start","Range contains only one peak start",
-               Color(111, 188, 112)),
-    PEAK_END("peakEnd",
-             "Peak End","Range contains only one peak end",
-             Color(255, 76, 76)),
+    PEAKS("peaks",
+          "Peaks","Range contains at least 1 peak",
+          Color(164, 69, 238)),
+
     NO_PEAKS("noPeaks",
              "No Peaks", "Range doesn't contain peaks",
              Color.lightGray),
-    PEAKS("peaks",
-          "Peaks","Range contains at least 1 peak",
-          Color(164, 69, 238));
+
+    PEAK_START("peakStart",
+               "Peak Start","Range contains only one peak start",
+               Color(111, 188, 112)),
+
+    PEAK_END("peakEnd",
+             "Peak End","Range contains only one peak end",
+             Color(255, 76, 76));
 
     override fun toString() = id
 
