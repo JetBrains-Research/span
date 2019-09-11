4b55d175a22ad99a217b28bce8366f866e1a22bd
dievsky <dievsky@gmail.com>
2019-09-11 11:40:04 +0200

    Added output test for Span2

--- src/test/kotlin/org/jetbrains/bio/span/Span2CLALongTest.kt
+++ src/test/kotlin/org/jetbrains/bio/span/Span2CLALongTest.kt
@@ -1,7 +1,10 @@
 package org.jetbrains.bio.span
 
 import org.jetbrains.bio.Configuration
+import org.jetbrains.bio.Tests
+import org.jetbrains.bio.experiments.fit.SpanModel
 import org.jetbrains.bio.genome.Genome
+import org.jetbrains.bio.io.BedFormat
 import org.jetbrains.bio.query.reduceIds
 import org.jetbrains.bio.query.stemGz
 import org.jetbrains.bio.statistics.distribution.Sampling
@@ -10,6 +13,7 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class Span2CLALongTest {
@@ -25,6 +29,50 @@ class Span2CLALongTest {
         SpanCLA.ignoreConfigurePaths = false
         // we might have unfinished tracked tasks which will never be complete, let's drop them
         MultitaskProgress.clear()
+    }
+
+    @Test
+    fun testOutput() {
+        // NOTE[oshpynov] we use .bed.gz here for the ease of sampling result save
+        withTempFile("track", ".bed.gz") { path ->
+
+            SpanCLALongTest.sampleCoverage(path, SpanCLALongTest.TO, SpanCLALongTest.BIN, goodQuality = true)
+            println("Saved sampled track file: $path")
+
+            withTempDirectory("work") {
+                val chromsizes = Genome["to1"].chromSizesPath.toString()
+                val peaksPath = path.parent / "${path.stem}.peak"
+                val (out, _) = Logs.captureLoggingOutput {
+                    SpanCLA.main(arrayOf("analyze", "-cs", chromsizes,
+                        "--workdir", it.toString(),
+                        "-t", path.toString(),
+                        "--threads", SpanCLALongTest.THREADS.toString(),
+                        "--type", "prm",
+                        "--peaks", peaksPath.toString(),
+                        "--fdr", "0.01"))
+                }
+                assertFalse("""NO output path given, process model fitting only.
+    LABELS, FDR, GAP options are ignored.
+    """ in out)
+                Tests.assertIn("100.00% (", out)
+                Tests.assertIn("MODEL TYPE: ${SpanModel.POISSON_REGRESSION_MIXTURE}", out)
+                Tests.assertIn("Track source: $peaksPath", out)
+                Tests.assertIn("FRIP: ", out)
+
+                /* Check that coverage is being generated */
+                val format = BedFormat.from("bed6+3")
+                assertTrue(
+                    format.parse(peaksPath) { parser ->
+                        parser.all { entry ->
+                            val coverage = entry.unpack(6).extraFields?.get(0)
+                            return@all coverage != null && coverage != "0.0"
+                        }
+                    },
+                    "Peak value is reported as 0.0, although the coverage cache is present"
+                )
+                Tests.assertIn("Signal to noise: ", out)
+            }
+        }
     }
 
     @Test
