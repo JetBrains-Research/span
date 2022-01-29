a52db4c225e3da10f20a24841bcb62fda232157a
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2022-01-29 21:13:57 +0300

    Mixture SPAN model for comparison with SPAN HMM

---
+++ src/main/kotlin/org/jetbrains/bio/span/statistics/mixture/NegBinMixture.kt
@@ -0,0 +1,107 @@
+package org.jetbrains.bio.span.statistics.mixture
+
+import org.jetbrains.bio.dataframe.DataFrame
+import org.jetbrains.bio.span.statistics.emission.NegBinEmissionScheme
+import org.jetbrains.bio.span.statistics.hmm.FreeNBZHMM
+import org.jetbrains.bio.statistics.Preprocessed
+import org.jetbrains.bio.statistics.emission.ConstantIntegerEmissionScheme
+import org.jetbrains.bio.statistics.emission.IntegerEmissionScheme
+import org.jetbrains.bio.statistics.mixture.MLFreeMixture
+import org.jetbrains.bio.statistics.model.Fitter
+import org.jetbrains.bio.viktor.F64Array
+import org.jetbrains.bio.viktor.asF64Array
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Mixture of three components:
+ * 0 - NULL (zero component)
+ * 1 - LOW (negative binomial regression with low mean)
+ * 2 - HIGH (negative binomial regression with high mean)
+ *
+ * @author Oleg Shpynov
+ * @date 1/29/22
+ */
+class NegBinMixture(
+    nbMeans: DoubleArray, nbFailures: DoubleArray,
+    weights: F64Array,
+) : MLFreeMixture(numComponents = 3, numDimensions = 1, weights = weights) {
+
+    private val zeroEmission: ConstantIntegerEmissionScheme = ConstantIntegerEmissionScheme(0)
+    private val negBinEmissionSchemes: Array<NegBinEmissionScheme> =
+        Array(nbMeans.size) { NegBinEmissionScheme(nbMeans[it], nbFailures[it]) }
+
+    override fun getEmissionScheme(i: Int, d: Int): IntegerEmissionScheme {
+        require(d == 0) { "Invalid dimension $d" }
+        return if (i == 0) zeroEmission else negBinEmissionSchemes[i - 1]
+    }
+
+    operator fun get(i: Int): IntegerEmissionScheme {
+        return if (i == 0) zeroEmission else negBinEmissionSchemes[i - 1]
+    }
+
+    operator fun set(i: Int, e: NegBinEmissionScheme) {
+        if (i == 0) {
+            throw IllegalArgumentException()
+        } else {
+            negBinEmissionSchemes[i - 1] = e
+        }
+    }
+
+    val means: F64Array get() = F64Array(negBinEmissionSchemes.size) { negBinEmissionSchemes[it].mean }
+
+    val failures: F64Array get() = F64Array(negBinEmissionSchemes.size) { negBinEmissionSchemes[it].failures }
+
+    override fun fit(preprocessed: List<Preprocessed<DataFrame>>, title: String, threshold: Double, maxIter: Int) {
+        val data = DataFrame.rowBind(preprocessed.map { it.get() }.toTypedArray())
+        super.fit(Preprocessed.of(data), title, threshold, maxIter)
+        flipStatesIfNecessary()
+    }
+
+    companion object {
+        @Suppress("MayBeConstant", "unused")
+        @Transient
+        @JvmField
+        var VERSION = 2
+
+        val LOG: Logger = LoggerFactory.getLogger(NegBinMixture::class.java)
+
+        fun fitter() = object : Fitter<NegBinMixture> {
+            override fun guess(
+                preprocessed: Preprocessed<DataFrame>,
+                title: String,
+                threshold: Double,
+                maxIter: Int,
+                attempt: Int
+            ) = guess(listOf(preprocessed), title, threshold, maxIter, attempt)
+
+            override fun guess(
+                preprocessed: List<Preprocessed<DataFrame>>,
+                title: String,
+                threshold: Double,
+                maxIter: Int,
+                attempt: Int
+            ): NegBinMixture {
+                val (means, failures) = FreeNBZHMM.guess(preprocessed, 2, attempt)
+                return NegBinMixture(
+                    means, failures,
+                    doubleArrayOf(1 / 3.0, 1 / 3.0, 1 / 3.0).asF64Array()
+                )
+            }
+        }
+
+        internal fun NegBinMixture.flipStatesIfNecessary() {
+            val lowScheme = this[1] as NegBinEmissionScheme
+            val highScheme = this[2] as NegBinEmissionScheme
+            if (weights[1] < weights[2]) {
+                LOG.warn("After fitting the model, the wight of LOW state is lower than that of HIGH state.")
+                LOG.warn("This usually indicates that the states were flipped during fitting. We will now flip them back.")
+                this[2] = lowScheme
+                this[1] = highScheme
+                val tmp = logWeights[1]
+                logWeights[1] = logWeights[2]
+                logWeights[2] = tmp
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/org/jetbrains/bio/span/SpanMCLALongTest.kt
@@ -0,0 +1,141 @@
+package org.jetbrains.bio.span
+
+import kotlinx.support.jdk7.use
+import org.jetbrains.bio.Tests.assertIn
+import org.jetbrains.bio.experiment.Configuration
+import org.jetbrains.bio.genome.Genome
+import org.jetbrains.bio.genome.Location
+import org.jetbrains.bio.genome.containers.LocationsMergingList
+import org.jetbrains.bio.genome.containers.genomeMap
+import org.jetbrains.bio.span.SpanCLALongTest.Companion.BIN
+import org.jetbrains.bio.span.SpanCLALongTest.Companion.THREADS
+import org.jetbrains.bio.span.SpanCLALongTest.Companion.TO
+import org.jetbrains.bio.span.SpanCLALongTest.Companion.sampleCoverage
+import org.jetbrains.bio.span.fit.SpanModelType
+import org.jetbrains.bio.statistics.distribution.Sampling
+import org.jetbrains.bio.util.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.io.FileReader
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class SpanMCLALongTest {
+
+    @Before
+    fun setUp() {
+        SpanCLA.ignoreConfigurePaths = true
+        Sampling.RANDOM_DATA_GENERATOR.randomGenerator.setSeed(1234L)
+    }
+
+    @After
+    fun tearDown() {
+        SpanCLA.ignoreConfigurePaths = false
+        // we might have unfinished tracked tasks which will never be complete, let's drop them
+        MultitaskProgress.clear()
+    }
+
+    @Test
+    fun testFilesCreatedByAnalyze() {
+        withTempDirectory("work") { dir ->
+            withTempFile("track", ".bed.gz", dir) { path ->
+                withTempFile("control", ".bed.gz", dir) { control ->
+                    // NOTE[oshpynov] we use .bed.gz here for the ease of sampling result save
+                    sampleCoverage(path, TO, BIN, goodQuality = true)
+                    sampleCoverage(control, TO, BIN, goodQuality = false)
+
+                    val chromsizes = Genome["to1"].chromSizesPath.toString()
+                    SpanCLA.main(
+                        arrayOf(
+                            "experimental",
+                            "-cs", chromsizes,
+                            "--workdir", dir.toString(),
+                            "-t", path.toString(),
+                            "-c", control.toString(),
+                            "--threads", THREADS.toString(),
+                            "--model-type", SpanModelType.NB2Z_MIXTURE.id
+                        )
+                    )
+
+                    // Model test
+                    assertTrue((Configuration.experimentsPath / "fit").exists)
+                    assertEquals(
+                        1,
+                        (Configuration.experimentsPath / "fit")
+                            .glob(
+                                "${
+                                    reduceIds(
+                                        listOf(
+                                            path.stemGz,
+                                            control.stemGz,
+                                            "200"
+                                        )
+                                    )
+                                }.${SpanModelType.NB2Z_MIXTURE.extension}"
+                            ).size
+                    )
+                }
+            }
+        }
+    }
+
+
+    @Test
+    fun analyzeNB2ZMixtureSampledEnrichment() {
+        withTempFile("track", ".bed.gz") { path ->
+            val enrichedRegions = genomeMap(TO) {
+                val enriched = BitSet()
+                if (it.name == "chr1") {
+                    enriched.set(1000, 2000)
+                }
+                enriched
+            }
+
+            val zeroRegions = genomeMap(TO) {
+                val zeroes = BitSet()
+                if (it.name == "chr1") {
+                    zeroes[3000] = 4000
+                }
+                zeroes
+            }
+            sampleCoverage(path, TO, BIN, enrichedRegions, zeroRegions, goodQuality = true)
+            println("Saved sampled track file: $path")
+            withTempDirectory("work") { dir ->
+                val bedPath = dir / "result.bed"
+                SpanCLA.main(
+                    arrayOf(
+                        "experimental",
+                        "--debug",
+                        "-cs", Genome["to1"].chromSizesPath.toString(),
+                        "-w", dir.toString(),
+                        "--peaks", bedPath.toString(),
+                        "-fdr", "0.05",
+                        "-t", path.toString(),
+                        "--model-type", SpanModelType.NB2Z_MIXTURE.id
+                    )
+                )
+                // Check created bed file
+                assertTrue(
+                    Location(1100 * BIN, 1900 * BIN, TO.get().first())
+                            in LocationsMergingList.load(TO, bedPath),
+                    "Expected location not found in called peaks"
+                )
+                // Check correct log file name
+                val logPath = dir / "logs" / "${bedPath.stem}.log"
+                assertTrue(logPath.exists, "Log file not found")
+                val log = FileReader(logPath.toFile()).use { it.readText() }
+                assertIn("Signal mean:", log)
+                assertIn("Noise mean:", log)
+                assertIn("Signal to noise:", log)
+                /** In sampling producer model - signal to noise ratio ~ 30
+                 * "mean": 0.36580807929296383, // noise
+                 * "mean": 9.113896687733767,   // signal
+                 */
+                assertTrue(log.substringAfter("Signal to noise:").substringBefore("\n").trim().toDouble() > 10)
+            }
+        }
+    }
+
+}
