dcf76a0b5033184c74dc17c83f92543ec0b5ec08
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2022-02-19 23:34:22 +0300

    Better multistart guess

--- src/main/kotlin/org/jetbrains/bio/span/statistics/emission/NegBinUtil.kt
+++ src/main/kotlin/org/jetbrains/bio/span/statistics/emission/NegBinUtil.kt
@@ -27,21 +27,20 @@ object NegBinUtil {
         val fs = NegativeBinomialDistribution.estimateFailuresUsingMoments(mean, max(1.1 * mean, sd * sd))
         val snr = multiStartSignalToNoise(attempt)
         // Make means and failures inverse proportional
-        val means = DoubleArray(n) { mean / snr.pow((n / 2 - it).toDouble()) }
+        val means = DoubleArray(n) { mean * snr.pow(((it + 1) / n).toDouble()) }
         // NegativeBinomialDistribution requires variance greater than mean, tweak variance if required.
         // Otherwise, failures will be set to +Inf and won't be updated during EM steps.
-        val failures = DoubleArray(n) { max(means[it] * 1.1, fs * snr.pow((n / 2 - it).toDouble())) }
+        val failures = DoubleArray(n) { max(means[it] * 1.1, fs * snr.pow(((it - n) / n).toDouble())) }
         LOG.debug("Guess $attempt emissions ${means.joinToString(",")}, failures ${failures.joinToString(",")}")
         return means to failures
     }
 
     /**
      * Propose initial signal-to-noise ration in multi-start runs.
-     * Good experiment signal-to-noise ratio is generally 10-30.
      * [snr] and [multiplier] as used to yield the sequence of values
      * snr, snr / multiplier, snr * multiplier, snr / multiplier^2, snr * multiplier^2, ...
      */
-    fun multiStartSignalToNoise(attempt: Int, snr: Double = 20.0, multiplier: Double = 2.0, min: Double = 1.1) =
+    fun multiStartSignalToNoise(attempt: Int, snr: Double = 10.0, multiplier: Double = 2.0, min: Double = 1.1) =
         max(min, snr * multiplier.pow(floor((attempt + 1) / 2.0) * (-1.0).pow(attempt + 1)))
 
 }
--- src/test/kotlin/org/jetbrains/bio/span/statistics/emissions/NegBitUtilTest.kt
+++ src/test/kotlin/org/jetbrains/bio/span/statistics/emissions/NegBitUtilTest.kt
@@ -9,7 +9,7 @@ class NegBitUtilTest {
     @Test
     fun testSNR() {
         val snrs = DoubleArray(5) { NegBinUtil.multiStartSignalToNoise(it) }
-        Assert.assertTrue(doubleArrayOf(20.0, 40.0, 10.0, 80.0, 5.0).contentEquals(snrs))
+        Assert.assertTrue(doubleArrayOf(10.0, 20.0, 5.0, 40.0, 2.5).contentEquals(snrs))
     }
 
     @Test
@@ -18,26 +18,9 @@ class NegBitUtilTest {
         val (means0, failures0) = NegBinUtil.guessByData(
             emissions, 2, 0
         )
-        assertEquals(3.51, means0[0], 1e-2)
-        assertEquals(70.33, means0[1], 1e-2)
-        assertEquals(11.64, failures0[0], 1e-2)
-        assertEquals(77.36, failures0[1], 1e-2)
-
-        val (means3, failures3) = NegBinUtil.guessByData(
-            emissions, 2, 3
-        )
-        assertEquals(0.87, means3[0], 1e-2)
-        assertEquals(70.33, means3[1], 1e-2)
-        assertEquals(46.59, failures3[0], 1e-2)
-        assertEquals(77.36, failures3[1], 1e-2)
-
-        val (means4, failures4) = NegBinUtil.guessByData(
-            emissions, 2, 4
-        )
-        assertEquals(14.06, means4[0], 1e-2)
-        assertEquals(70.33, means4[1], 1e-2)
-        assertEquals(15.47, failures4[0], 1e-2)
-        assertEquals(77.36, failures4[1], 1e-2)
-
+        assertEquals(70.33, means0[0], 1e-2)
+        assertEquals(703.33, means0[1], 1e-2)
+        assertEquals(77.36, failures0[0], 1e-2)
+        assertEquals(773.66, failures0[1], 1e-2)
     }
 }
