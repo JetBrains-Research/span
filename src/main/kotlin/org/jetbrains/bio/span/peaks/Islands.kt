193fceb8de3bc6bdafdcff745a1f42da0b957092
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2022-02-21 14:13:40 +0300

    Cleanup: removed unused summitsOnly feature

--- src/main/kotlin/org/jetbrains/bio/span/peaks/Islands.kt
+++ src/main/kotlin/org/jetbrains/bio/span/peaks/Islands.kt
@@ -9,21 +9,19 @@ import org.jetbrains.bio.span.fit.SpanFitResults
 import org.jetbrains.bio.span.fit.SpanModelFitExperiment
 import org.jetbrains.bio.statistics.f64Array
 import org.jetbrains.bio.statistics.hypothesis.Fdr
-import org.jetbrains.bio.statistics.hypothesis.StofferLiptakTest
 import org.jetbrains.bio.util.CancellableState
 import org.jetbrains.bio.util.Progress
 import org.jetbrains.bio.viktor.F64Array
 import org.jetbrains.bio.viktor.KahanSum
 import java.lang.Integer.max
 import java.util.concurrent.atomic.AtomicInteger
-import kotlin.math.exp
 import kotlin.math.ln
 import kotlin.math.min
 
 
 /**
  * The islands are called in five steps.
-*
+ *
  * 1) Estimate posterior probabilities
  * 2) Pick candidate bins with relaxed posterior error probability, e.g. sqrt(fdr).
  * This mitigates the problem of wide marks peaks split on strong fdrs.
@@ -32,12 +30,11 @@ import kotlin.math.min
  *    Each block is assigned P as average posterior log error probability for bins in blocks.
  *    50% top significant blocks scores are aggregated using length-weighted average as P for island.
  * 5) Compute qvalues on islands p-values, filter by alpha.
-*/
+ */
 internal fun SpanFitResults.getIslands(
     genomeQuery: GenomeQuery,
     fdr: Double,
     gap: Int,
-    summitsOnly: Boolean = false,
     cancellableState: CancellableState? = null
 ): List<Peak> {
     resetCounters()
@@ -45,7 +42,7 @@ internal fun SpanFitResults.getIslands(
     fitInfo.prepareScores()
     val map = genomeMap(genomeQuery, parallel = true) { chromosome ->
         cancellableState?.checkCanceled()
-        val chromosomeIslands = getChromosomeIslands(chromosome, fdr, gap, summitsOnly)
+        val chromosomeIslands = getChromosomeIslands(chromosome, fdr, gap)
         progress.report(1)
         chromosomeIslands
     }
@@ -61,7 +58,6 @@ internal fun SpanFitResults.getChromosomeIslands(
     chromosome: Chromosome,
     fdr: Double,
     gap: Int,
-    summitsOnly: Boolean = false
 ): List<Peak> {
     // Check that we have information for requested chromosome
     val chromosomeIslands = if (chromosome.name in fitInfo.chromosomesSizes) {
@@ -71,7 +67,6 @@ internal fun SpanFitResults.getChromosomeIslands(
             chromosome,
             fdr,
             gap,
-            summitsOnly
         )
     } else {
         SpanFitResults.LOG.debug(
@@ -98,7 +93,6 @@ private fun SpanFitResults.getChromosomeIslands(
     chromosome: Chromosome,
     fdr: Double,
     gap: Int,
-    summitsOnly: Boolean,
     relaxPower: Double = RELAX_POWER_DEFAULT,
 ): List<Peak> {
     /*
@@ -122,9 +116,6 @@ private fun SpanFitResults.getChromosomeIslands(
         return emptyList()
     }
 
-    // Init Stoffer-Liptak test to correct dependence between consequent p-values if required
-    val slTest = if (summitsOnly) StofferLiptakTest(logNullMemberships.exp().toDoubleArray()) else null
-
     /**
      * We want two invariants from islands pvalues:
      * 1) The more strict FDR, the fewer peaks with smaller average length
@@ -132,25 +123,13 @@ private fun SpanFitResults.getChromosomeIslands(
      *
      * Peak score is computed as length-weighted average p-value in its consequent enriched bins.
      */
-    val islandsLogPeps = F64Array(candidateIslands.size) { islandIndex ->
+    val islandsLogPs = F64Array(candidateIslands.size) { islandIndex ->
         val blocks = strictBins.findConsequentBlocks(candidateIslands[islandIndex])
-        val blocksLogPeps = blocks.map { (from, to) ->
-            if (summitsOnly) {
-                // Estimate only summits by sliding window in blocks - looking for summit with
-                // the most significant Stoffer-Liptak test combined p-value
-                val slidingWindowCombinedPs = SUMMIT_WINDOWS.flatMap { window ->
-                    (max(0, from - window + 1)..min(to, offsets.size - window)).map { start ->
-                        ln(slTest!!.combine(DoubleArray(window) { exp(logNullMemberships[start + it]) }))
-                    }
-                }
-                // Minimum log p-value for any window size
-                slidingWindowCombinedPs.minOrNull() ?: 1.0
-            } else {
-                // Average posterior log error probability for block
-                KahanSum().apply {
-                    (from until to).forEach { feed(logNullMemberships[it]) }
-                }.result() / (to - from)
-            }
+        val blocksLogPs = blocks.map { (from, to) ->
+            // Average posterior log error probability for block
+            KahanSum().apply {
+                (from until to).forEach { feed(logNullMemberships[it]) }
+            }.result() / (to - from)
         }
 
         /**
@@ -161,7 +140,7 @@ private fun SpanFitResults.getChromosomeIslands(
          */
         val sum = KahanSum()
         var l = 0
-        blocks.zip(blocksLogPeps).sortedBy { it.second }.take(max(1, blocks.size / 2)).forEach { (b, p) ->
+        blocks.zip(blocksLogPs).sortedBy { it.second }.take(max(1, blocks.size / 2)).forEach { (b, p) ->
             sum += p * (b.toIndex - b.fromIndex)
             l += b.toIndex - b.fromIndex
         }
@@ -169,7 +148,7 @@ private fun SpanFitResults.getChromosomeIslands(
     }
 
     // Filter result islands by Q values
-    val islandsLogQValues = Fdr.qvalidate(islandsLogPeps, logResults = true)
+    val islandsLogQValues = Fdr.qvalidate(islandsLogPs, logResults = true)
     val resultIslandsIndexes = candidateIslands.indices.filter {
         islandsLogQValues[it] < logFdr
     }
@@ -181,7 +160,7 @@ private fun SpanFitResults.getChromosomeIslands(
             chromosome = chromosome,
             startOffset = start,
             endOffset = end,
-            mlogpvalue = -islandsLogPeps[idx] / LOG_10,
+            mlogpvalue = -islandsLogPs[idx] / LOG_10,
             mlogqvalue = -islandsLogQValues[idx] / LOG_10,
             // Value is either coverage of fold change
             value = fitInfo.score(ChromosomeRange(start, end, chromosome)),
@@ -202,4 +181,3 @@ private fun SpanFitResults.getChromosomeIslands(
 fun relaxedLogFdr(logFdr: Double, relaxPower: Double) = min(ln(0.1), logFdr * relaxPower)
 
 const val RELAX_POWER_DEFAULT = 0.5
-val SUMMIT_WINDOWS = intArrayOf(2, 3, 5)
